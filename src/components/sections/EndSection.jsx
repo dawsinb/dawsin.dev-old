258c9333619b6023608a93771c145af98f80ec35
dawsin <dawsin.blanchard@maine.edu>
2021-12-01 22:19:05 -0500

    end text transition effect

--- src/components/sections/EndSection.jsx
+++ src/components/sections/EndSection.jsx
@@ -1,10 +1,12 @@
-import React, { useRef, useLayoutEffect } from "react";
-import { FontLoader } from "three"
-import { useThree } from "@react-three/fiber";
+import React, { useRef, useEffect, useState } from "react";
+import { FontLoader, TextGeometry, BufferAttribute, BufferGeometry } from "three"
+import { useThree, useFrame } from "@react-three/fiber";
 import { Text } from "@react-three/drei";
 import { useStore } from "../../store";
 import { Section, SectionItem } from "./Section";
 import BorisBlackBloxx from "../../fonts/BorisBlackBloxx.json"
+import Yuji_Syuku from "../../fonts/Yuji_Syuku.json"
+import { lerp } from "../../utils";
 
 function EndSection({ index, parallax }) {
   const isMobile = false;
@@ -30,25 +32,117 @@ function EndSection({ index, parallax }) {
   const textY = fontSize / 2;
   const textPosition = [textX, textY, 0];
   
+  //
 
   const font = new FontLoader().parse(BorisBlackBloxx)
+  const fontJp = new FontLoader().parse(Yuji_Syuku)
 
+  const nineGeometry = new TextGeometry("nine", {
+    font: font,
+    size: fontSize,
+    height: 0.01
+  })
+  nineGeometry.center()
 
-  const center = (self) => self.center();
+  const nineGeometryJp = new TextGeometry("九", {
+    font: fontJp,
+    size: fontSize * 1.25,
+    height: 0.01
+  })
+  nineGeometryJp.center()
+
+  const nineBufferSize = Math.max(nineGeometry.attributes.position.count, nineGeometryJp.attributes.position.count) * 3
+
+  const nineBuffer = new Float32Array(nineBufferSize)
+  nineBuffer.set(nineGeometry.attributes.position.array)
+
+  const nineBufferJp = new Float32Array(nineBufferSize)
+  nineBufferJp.set(nineGeometryJp.attributes.position.array)
+
+  nineGeometry.setAttribute("position", new BufferAttribute(nineBuffer, 3))
+  nineGeometry.morphAttributes.position = [new BufferAttribute(nineBufferJp, 3)]
+  console.log(nineGeometry.attributes.position)
+
+  const fourGeometry = new TextGeometry("four", {
+    font: font,
+    size: fontSize,
+    height: 0.01
+  })
+  fourGeometry.center()
+
+  const fourGeometryJp = new TextGeometry("四", {
+    font: fontJp,
+    size: fontSize * 1.25,
+    height: 0.01
+  })
+  fourGeometryJp.center()
+
+  const fourBufferSize = Math.max(fourGeometry.attributes.position.count, fourGeometryJp.attributes.position.count) * 3
+
+  const fourBuffer = new Float32Array(fourBufferSize)
+  fourBuffer.set(fourGeometry.attributes.position.array)
+
+  const fourBufferJp = new Float32Array(fourBufferSize)
+  fourBufferJp.set(fourGeometryJp.attributes.position.array)
+
+  fourGeometry.setAttribute("position", new BufferAttribute(fourBuffer, 3))
+  fourGeometry.morphAttributes.position = [new BufferAttribute(fourBufferJp, 3)]
+  //
+
+  const nineRef = useRef()
+  const fourRef = useRef()
+  useEffect(() => {
+    nineRef.current.updateMorphTargets()
+    fourRef.current.updateMorphTargets()
+  }, [])
+
+  const [nineHovered, setNineHovered] = useState(false)
+  const [fourHovered, setFourHovered] = useState(false)
+
+  const [nineState, setNineState] = useState(false)
+  const [fourState, setFourState] = useState(false)
+
+  useFrame(() => {
+    nineRef.current.morphTargetInfluences[0] = lerp(nineRef.current.morphTargetInfluences[0], nineHovered ? 0.5 : Number(nineState), 0.03)
+    fourRef.current.morphTargetInfluences[0] = lerp(fourRef.current.morphTargetInfluences[0], fourHovered ? 0.5 : Number(fourState), 0.03)
+  })
 
   return (
     <Section index={index} parallax={parallax} height={sectionHeight}>
       <SectionItem parallax={2}>
         <group position={textPosition}>
-          <mesh>
-            <textGeometry onUpdate={center} args={["nine", {font: font, size: fontSize, height: 1}]}/>
-            <meshBasicMaterial color={secondary} />
-          </mesh>
+          <group>
+            <mesh
+              ref={nineRef}
+              geometry={nineGeometry}
+            >
+              <meshBasicMaterial color={secondary} />
+            </mesh>
+            <mesh
+              onPointerEnter={() => {setNineHovered(true); setNineState(!nineState)}}
+              onPointerOut={() => {setNineHovered(false)}}
+            >
+              <planeGeometry args={[fontSize * 4, fontSize * 1.5]}/>
+              <meshBasicMaterial transparent opacity={0}/>
+            </mesh>
+          </group>
 
-          <mesh position={[0, -fontSize / 1.3, 0]}>
-            <textGeometry onUpdate={center} args={["four", {font: font, size: fontSize, height: 1}]}/>
-            <meshBasicMaterial color={primary} />
-          </mesh>
+          <group position={[0, -fontSize / 1.3, 1]}>
+            <mesh 
+              ref={fourRef}
+              geometry={fourGeometry}
+            >
+              <meshBasicMaterial color={primary} />
+            </mesh>
+            <mesh
+                onPointerEnter={() => {setFourHovered(true); setFourState(!fourState)}}
+                onPointerOut={() => {setFourHovered(false)}}
+              >
+                <planeGeometry args={[fontSize * 4, fontSize * 1.5]}/>
+                <meshBasicMaterial transparent opacity={0}/>
+              </mesh>
+          </group>
+          
         </group>
       </SectionItem>
 
@@ -64,11 +158,3 @@ function EndSection({ index, parallax }) {
 
 export { EndSection };
 
-/*
-<Text font={"/fonts/SawarabiMincho.ttf"} fontSize={fontSize} color={primary} anchorX={"50%"} anchorY={"90%"}>
-  九
-</Text>
-<Text font={"/fonts/SawarabiMincho.ttf"} fontSize={fontSize} color={secondary} anchorX={"50%"} anchorY={"15%"}>
-  四
-</Text>
-*/
